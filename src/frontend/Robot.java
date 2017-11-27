4135e9d8dfc8109064c6876350b7bddfc00251dc
Michael Pu <michael.pu123@gmail.com>
2017-11-26 21:05:51 -0500

    updated gui

--- src/frontend/Robot.java
+++ src/frontend/Robot.java
@@ -6,6 +6,8 @@
 
 package frontend;
 
+import java.awt.Point;
+
 import algorithm.Direction;
 import algorithm.Path;
 import backend.Coordinate;
@@ -16,107 +18,105 @@ import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-import java.awt.*;
-
 public class Robot extends Sprite {
 
-    private static final String IMAGE_ROBOT = Utilities.IMAGE_DIRECTORY + "robot.png";
+	private static final String IMAGE_ROBOT = Utilities.IMAGE_DIRECTORY + "robot.png";
 
-    private static final double MOVE_DURATION = 0.5;
-    private static final Color VISITED_CELL_COLOUR = Color.YELLOW;
+	private static final double MOVE_DURATION = 0.5;
+	private static final Color VISITED_CELL_COLOUR = Color.YELLOW;
 
-    private ImageView mImageView;
-    private Path mPath;
-    private double mSquareSideLength;
-    private Direction mCurrentDirection;
-    private Coordinate mPositionBeforeMove;
-    private Point mPositionInGraphAfterMove;
-    private double mMoveCountdown = MOVE_DURATION;
-    private boolean mDoneMoving = false;
-    private int mFacingDirection = 0;
-    private Rectangle[][] mMazeRect;
-    private int mNumMoves = 0;
+	private ImageView mImageView;
+	private Path mPath;
+	private double mSquareSideLength;
+	private Direction mCurrentDirection;
+	private Coordinate mPositionBeforeMove;
+	private Point mPositionInGraphAfterMove;
+	private double mMoveCountdown = MOVE_DURATION;
+	private boolean mDoneMoving = false;
+	private int mFacingDirection = 0;
+	private Rectangle[][] mMazeRect;
+	private int mNumMoves = 0;
 
-    public Robot(ImageView image, Path path, Point startingPosition, double squareSideLength, Rectangle[][] mazeRect) {
+	public Robot(ImageView image, Path path, Point startingPosition, double squareSideLength, Rectangle[][] mazeRect) {
 
-        super(image);
+		super(image);
 
-        double imageMaxDimension = squareSideLength * 0.8;
+		double imageMaxDimension = squareSideLength * 0.8;
 
-        System.out.println(startingPosition.x + " " + startingPosition.y);
+		mImageView = (ImageView) mNode;
+		mImageView.setImage(new Image(Utilities.getResourceAsStream(IMAGE_ROBOT)));
+		mImageView.setPreserveRatio(true);
+		if (mImageView.getFitWidth() > mImageView.getFitHeight()) {
+			mImageView.setFitWidth(imageMaxDimension);
+		} else {
+			mImageView.setFitHeight(imageMaxDimension);
+		}
 
-        mImageView = (ImageView) mNode;
-        mImageView.setImage(new Image(Utilities.getResourceAsStream(IMAGE_ROBOT)));
-        mImageView.setPreserveRatio(true);
-        if (mImageView.getFitWidth() > mImageView.getFitHeight()) {
-            mImageView.setFitWidth(imageMaxDimension);
-        } else {
-            mImageView.setFitHeight(imageMaxDimension);
-        }
+		mPath = path;
+		mMazeRect = mazeRect;
+		mSquareSideLength = squareSideLength;
+		mPositionInGraphAfterMove = (Point) startingPosition.clone();
+		setCentreOfImage(new Coordinate(startingPosition.x * mSquareSideLength + mSquareSideLength / 2.0,
+				startingPosition.y * mSquareSideLength + mSquareSideLength / 2.0));
 
-        mPath = path;
-        mMazeRect = mazeRect;
-        mSquareSideLength = squareSideLength;
-        mPositionInGraphAfterMove = (Point) startingPosition.clone();
-        setCentreOfImage(new Coordinate(startingPosition.x * mSquareSideLength + mSquareSideLength / 2.0, startingPosition.y * mSquareSideLength + mSquareSideLength / 2.0));
+		updateImageViewPosition();
+	}
 
-        updateImageViewPosition();
-    }
+	private void updateImageViewPosition() {
+		mImageView.setX(mPosition.getX());
+		mImageView.setY(mPosition.getY());
+	}
 
-    private void updateImageViewPosition() {
-        mImageView.setX(mPosition.getX());
-        mImageView.setY(mPosition.getY());
-    }
+	private void setCentreOfImage(Coordinate centrePosition) {
+		double width = mImageView.getBoundsInParent().getWidth();
+		double height = mImageView.getBoundsInParent().getHeight();
+		mPosition.setX(centrePosition.getX() - (width / 2));
+		mPosition.setY(centrePosition.getY() - (height / 2));
+		updateImageViewPosition();
+	}
 
-    private void setCentreOfImage(Coordinate centrePosition) {
-        double width = mImageView.getBoundsInParent().getWidth();
-        double height = mImageView.getBoundsInParent().getHeight();
-        mPosition.setX(centrePosition.getX() - (width / 2));
-        mPosition.setY(centrePosition.getY() - (height / 2));
-        updateImageViewPosition();
-    }
+	@Override
+	public void update(double deltaTime) {
+		if (!mDoneMoving) {
+			mMoveCountdown += deltaTime;
+			if (mMoveCountdown >= MOVE_DURATION) {
 
-    @Override
-    public void update(double deltaTime) {
-        if (!mDoneMoving) {
-            mMoveCountdown += deltaTime;
-            if (mMoveCountdown >= MOVE_DURATION) {
+				mMoveCountdown = 0;
+				mNumMoves++;
 
-                mMoveCountdown = 0;
-                mNumMoves++;
+				double beforeMoveCentreX = mPositionInGraphAfterMove.x * mSquareSideLength + mSquareSideLength / 2.0;
+				double beforeMoveCentreY = mPositionInGraphAfterMove.y * mSquareSideLength + mSquareSideLength / 2.0;
+				mPositionBeforeMove = new Coordinate(beforeMoveCentreX, beforeMoveCentreY);
 
-                double beforeMoveCentreX = mPositionInGraphAfterMove.x * mSquareSideLength + mSquareSideLength / 2.0;
-                double beforeMoveCentreY = mPositionInGraphAfterMove.y * mSquareSideLength + mSquareSideLength / 2.0;
-                mPositionBeforeMove = new Coordinate(beforeMoveCentreX, beforeMoveCentreY);
+				mMazeRect[mPositionInGraphAfterMove.x][mPositionInGraphAfterMove.y].setFill(VISITED_CELL_COLOUR);
 
-                mMazeRect[mPositionInGraphAfterMove.x][mPositionInGraphAfterMove.y].setFill(VISITED_CELL_COLOUR);
+				if (mPath.hasNext()) {
+					mCurrentDirection = mPath.getNext();
+					mPositionInGraphAfterMove.x += mCurrentDirection.change.x;
+					mPositionInGraphAfterMove.y += mCurrentDirection.change.y;
+					int imageRotateAmount = mFacingDirection - mCurrentDirection.direction;
+					mImageView.setRotate(imageRotateAmount);
 
-                if (mPath.hasNext()) {
-                    mCurrentDirection = mPath.getNext();
-                    mPositionInGraphAfterMove.x += mCurrentDirection.change.x;
-                    mPositionInGraphAfterMove.y += mCurrentDirection.change.y;
-                    int imageRotateAmount = mFacingDirection - mCurrentDirection.direction;
-                    mImageView.setRotate(imageRotateAmount);
+				} else {
+					mPosition = mPositionBeforeMove.copy();
+					mDoneMoving = true;
+					return;
+				}
+			}
 
-                } else {
-                    mPosition = mPositionBeforeMove.copy();
-                    mDoneMoving = true;
-                    return;
-                }
-            }
+			double offsetX = mCurrentDirection.change.x * mSquareSideLength * (mMoveCountdown / MOVE_DURATION);
+			double offsetY = mCurrentDirection.change.y * mSquareSideLength * (mMoveCountdown / MOVE_DURATION);
 
-            double offsetX = mCurrentDirection.change.x * mSquareSideLength * (mMoveCountdown / MOVE_DURATION);
-            double offsetY = mCurrentDirection.change.y * mSquareSideLength * (mMoveCountdown / MOVE_DURATION);
+			Coordinate newCentre = new Coordinate(mPositionBeforeMove.getX() + offsetX,
+					mPositionBeforeMove.getY() + offsetY);
 
-            Coordinate newCentre = new Coordinate(mPositionBeforeMove.getX() + offsetX, mPositionBeforeMove.getY() + offsetY);
+			setCentreOfImage(newCentre);
 
-            setCentreOfImage(newCentre);
+			updateImageViewPosition();
+		}
+	}
 
-            updateImageViewPosition();
-        }
-    }
-
-    public int getmNumMoves() {
-        return mNumMoves;
-    }
+	public int getmNumMoves() {
+		return mNumMoves;
+	}
 }
--- src/frontend/Window.java
+++ src/frontend/Window.java
@@ -6,7 +6,17 @@
 
 package frontend;
 
-import algorithm.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import algorithm.Cell;
+import algorithm.InvalidMapException;
+import algorithm.Maze;
+import algorithm.MazeParser;
+import algorithm.MazeSolver;
+import algorithm.Path;
 import backend.Sprite;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -30,267 +40,262 @@ import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-
 public class Window extends Application {
 
-    private static final double SCORE_BAR_HEIGHT = 40;
+	private static final double SCORE_BAR_HEIGHT = 40;
 
-    private long prevTime;
+	private long prevTime;
 
-    private Stage primaryStage;
-    private ArrayList<Sprite> allSpriteList;
+	private Stage primaryStage;
+	private ArrayList<Sprite> allSpriteList;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        primaryStage = stage;
-        openSelectMapPopUp();
-    }
+	@Override
+	public void start(Stage stage) throws Exception {
+		primaryStage = stage;
+		openSelectMapPopUp();
+	}
 
-    private void openSelectMapPopUp() {
+	private void openSelectMapPopUp() {
 
-        Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.initOwner(primaryStage);
-        dialog.setTitle("Select a Map");
-        dialog.setWidth(500);
-        dialog.setResizable(false);
-        dialog.setAlwaysOnTop(true);
+		Stage dialog = new Stage();
+		dialog.initModality(Modality.APPLICATION_MODAL);
+		dialog.initOwner(primaryStage);
+		dialog.setTitle("Select a Map");
+		dialog.setWidth(500);
+		dialog.setResizable(false);
+		dialog.setAlwaysOnTop(true);
 
-        VBox dialogRoot = new VBox(10);
-        Scene dialogScene = new Scene(dialogRoot);
-        dialogRoot.setAlignment(Pos.TOP_LEFT);
-        dialogRoot.setPadding(new Insets(15));
-        dialog.setScene(dialogScene);
+		VBox dialogRoot = new VBox(10);
+		Scene dialogScene = new Scene(dialogRoot);
+		dialogRoot.setAlignment(Pos.TOP_LEFT);
+		dialogRoot.setPadding(new Insets(15));
+		dialog.setScene(dialogScene);
 
-        File defaultDir = new File(System.getProperty("user.home"));
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setInitialDirectory(defaultDir);
-        fileChooser.setTitle("Open Map File");
+		File defaultDir = new File(System.getProperty("user.home"));
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setInitialDirectory(defaultDir);
+		fileChooser.setTitle("Open Map File");
 
-        OutputConsole outputConsole = new OutputConsole();
-        outputConsole.setPrefHeight(300);
-        outputConsole.setEditable(false);
-        outputConsole.setWrapText(true);
-        outputConsole.setInitalText("Please choose a map file to use.");
+		OutputConsole outputConsole = new OutputConsole();
+		outputConsole.setPrefHeight(300);
+		outputConsole.setEditable(false);
+		outputConsole.setWrapText(true);
+		outputConsole.setInitalText("Please choose a map file to use.");
 
-        HBox buttons = new HBox(10);
+		HBox buttons = new HBox(10);
 
-        Button selectFileButton = new Button("Choose File");
-        buttons.getChildren().add(selectFileButton);
+		Button selectFileButton = new Button("Choose File");
+		buttons.getChildren().add(selectFileButton);
 
-        Button startSimulationButton = new Button("Start Simulation");
-        startSimulationButton.setDisable(true);
-        buttons.getChildren().add(startSimulationButton);
+		Button startSimulationButton = new Button("Start Simulation");
+		startSimulationButton.setDisable(true);
+		buttons.getChildren().add(startSimulationButton);
 
-        Text fileText = new Text("No file has been selected.");
-        fileText.setWrappingWidth(dialog.getWidth() * 0.9);
-        fileText.setFill(Color.RED);
+		Text fileText = new Text("No file has been selected.");
+		fileText.setWrappingWidth(dialog.getWidth() * 0.9);
+		fileText.setFill(Color.RED);
 
-        dialogRoot.getChildren().add(outputConsole);
-        dialogRoot.getChildren().add(buttons);
-        dialogRoot.getChildren().add(fileText);
+		dialogRoot.getChildren().add(outputConsole);
+		dialogRoot.getChildren().add(buttons);
+		dialogRoot.getChildren().add(fileText);
 
-        dialog.show();
+		dialog.show();
 
-        selectFileButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
+		selectFileButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
 
-                Maze maze;
+				Maze maze;
 
-                File mapFile = fileChooser.showOpenDialog(dialog);
+				File mapFile = fileChooser.showOpenDialog(dialog);
 
-                if (mapFile == null) {
-                    return;
-                }
+				if (mapFile == null) {
+					return;
+				}
 
-                fileText.setText(mapFile.getAbsolutePath());
-                fileText.setFill(Color.BLACK);
+				fileText.setText(mapFile.getAbsolutePath());
+				fileText.setFill(Color.BLACK);
 
-                try {
-                    outputConsole.appendText("Reading file: " + mapFile.getCanonicalPath());
-                } catch (IOException e) {
-                    outputConsole.appendText(e.toString() + "\nInvalid file. Please select another file");
-                    fileText.setFill(Color.RED);
-                    return;
-                }
+				try {
+					outputConsole.appendText("Reading file: " + mapFile.getCanonicalPath());
+				} catch (IOException e) {
+					outputConsole.appendText(e.toString() + "\nInvalid file. Please select another file");
+					fileText.setFill(Color.RED);
+					return;
+				}
 
-                try {
-                    maze = loadMap(mapFile);
-                } catch (InvalidMapException e) {
-                    outputConsole.appendText(e.toString() + "\nInvalid map file. Please select another file.");
-                    fileText.setFill(Color.RED);
-                    return;
-                } catch (IOException e) {
-                    outputConsole.appendText(e.toString() + "\nInvalid path to map file. Please select another file");
-                    fileText.setFill(Color.RED);
-                    return;
-                }
+				try {
+					maze = loadMap(mapFile);
+				} catch (InvalidMapException e) {
+					outputConsole.appendText(e.toString() + "\nInvalid map file. Please select another file.");
+					fileText.setFill(Color.RED);
+					return;
+				} catch (IOException e) {
+					outputConsole.appendText(e.toString() + "\nInvalid path to map file. Please select another file");
+					fileText.setFill(Color.RED);
+					return;
+				}
 
-                outputConsole.appendText("Successfully read and parsed the map file.");
-                fileText.setFill(Color.GREEN);
-                outputConsole.appendText("Attempting to find shortest path in map...");
-                long startFindPath = System.nanoTime();
-                Path path = findPath(maze);
+				outputConsole.appendText("Successfully read and parsed the map file.");
+				fileText.setFill(Color.GREEN);
+				outputConsole.appendText("Attempting to find shortest path in map...");
+				long startFindPath = System.nanoTime();
+				Path path = findPath(maze);
 
-                if (path == null) {
-                    outputConsole.appendText("No path found. Please select another map.");
-                    fileText.setFill(Color.RED);
-                } else {
-                    double findPathTime = (System.nanoTime()-startFindPath) / 1E6;
-                    String findPathString = String.format("%.4f milliseconds", findPathTime);
-                    outputConsole.appendText("Path found in " + findPathString + ". Ready for simulation.");
-                    startSimulationButton.setDisable(false);
+				if (path == null) {
+					outputConsole.appendText("No path found. Please select another map.");
+					fileText.setFill(Color.RED);
+				} else {
+					double findPathTime = (System.nanoTime() - startFindPath) / 1E6;
+					String findPathString = String.format("%.4f milliseconds", findPathTime);
+					outputConsole.appendText("Path found in " + findPathString + ". Ready for simulation.");
+					startSimulationButton.setDisable(false);
 
-                    startSimulationButton.setOnAction(new EventHandler<ActionEvent>() {
-                        @Override
-                        public void handle(ActionEvent event) {
-                            openSimulation(maze, path);
-                            outputConsole.appendText("Opening simulation...");
-                            dialog.hide();
-                        }
-                    });
-                }
+					startSimulationButton.setOnAction(new EventHandler<ActionEvent>() {
+						@Override
+						public void handle(ActionEvent event) {
+							openSimulation(maze, path);
+							outputConsole.appendText("Opening simulation...");
+							dialog.hide();
+						}
+					});
+				}
 
-            }
-        });
+			}
+		});
 
-    }
+	}
 
-    private Maze loadMap(File mapFile) throws InvalidMapException, IOException {
-        String contents = new String(Files.readAllBytes(mapFile.toPath()));
-        return MazeParser.parseMaze(contents);
-    }
+	private Maze loadMap(File mapFile) throws InvalidMapException, IOException {
+		String contents = new String(Files.readAllBytes(mapFile.toPath()));
+		return MazeParser.parseMaze(contents);
+	}
 
-    private Path findPath(Maze maze) {
-        return MazeSolver.solve(maze);
-    }
+	private Path findPath(Maze maze) {
+		return MazeSolver.solve(maze);
+	}
 
-    private void openSimulation(Maze maze, Path path) {
+	private void openSimulation(Maze maze, Path path) {
 
-        allSpriteList = new ArrayList<>();
+		allSpriteList = new ArrayList<>();
 
-        BorderPane pane = new BorderPane();
-        Scene scene = new Scene(pane);
+		BorderPane pane = new BorderPane();
+		Scene scene = new Scene(pane);
 
-        Pane root = new Pane();
-        pane.setCenter(root);
+		Pane root = new Pane();
+		pane.setCenter(root);
 
-        HBox topBar = new HBox(5);
-        topBar.setPadding(new Insets(0, 5, 0, 5));
-        topBar.setPrefHeight(SCORE_BAR_HEIGHT);
-        topBar.setAlignment(Pos.CENTER_LEFT);
-        pane.setTop(topBar);
+		HBox topBar = new HBox(5);
+		topBar.setPadding(new Insets(0, 5, 0, 5));
+		topBar.setPrefHeight(SCORE_BAR_HEIGHT);
+		topBar.setAlignment(Pos.CENTER_LEFT);
+		pane.setTop(topBar);
 
-        Sprite.setSpriteArrayList(allSpriteList);
-        Sprite.setPane(topBar);
+		Sprite.setSpriteArrayList(allSpriteList);
+		Sprite.setPane(topBar);
 
-        Button restartButton = new Button("Restart Simulation");
-        topBar.getChildren().add(restartButton);
+		Button restartButton = new Button("Restart Simulation");
+		topBar.getChildren().add(restartButton);
 
-        Button otherMapButton = new Button("Select Another Map");
-        topBar.getChildren().add(otherMapButton);
+		Button otherMapButton = new Button("Select Another Map");
+		topBar.getChildren().add(otherMapButton);
 
-        ScoreIndicator scoreIndicator = new ScoreIndicator(new Text());
+		ScoreIndicator scoreIndicator = new ScoreIndicator(new Text());
 
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        double minWindowDimension = Math.min(primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight()-SCORE_BAR_HEIGHT) * 0.9;
-        double squareSideLength = minWindowDimension / Math.max(maze.getWidth(), maze.getHeight());
+		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+		double minWindowDimension = Math.min(primaryScreenBounds.getWidth(),
+				primaryScreenBounds.getHeight() - SCORE_BAR_HEIGHT) * 0.9;
+		double squareSideLength = minWindowDimension / Math.max(maze.getWidth(), maze.getHeight());
 
-        Sprite.setPane(root);
+		Sprite.setPane(root);
 
-        root.setPrefWidth(squareSideLength * maze.getWidth());
-        root.setPrefHeight(squareSideLength * maze.getHeight());
+		root.setPrefWidth(squareSideLength * maze.getWidth());
+		root.setPrefHeight(squareSideLength * maze.getHeight());
 
-        Rectangle[][] mazeRects = new Rectangle[maze.getWidth()][maze.getHeight()];
+		Rectangle[][] mazeRects = new Rectangle[maze.getWidth()][maze.getHeight()];
 
-        for (int col = 0; col < maze.getWidth(); col++) {
-            for (int row = 0; row < maze.getHeight(); row++) {
+		for (int col = 0; col < maze.getWidth(); col++) {
+			for (int row = 0; row < maze.getHeight(); row++) {
 
-                Cell curCell = maze.getCell(col, row);
-                Color squareColor = null;
+				Cell curCell = maze.getCell(col, row);
+				Color squareColor = null;
 
-                switch (curCell) {
-                    case GOAL:
-                        squareColor = Color.RED;
-                        break;
-                    case START:
-                        squareColor = Color.GREEN;
-                        break;
-                    case EMPTY:
-                        squareColor = Color.LIGHTGREY;
-                        break;
-                    case WALL:
-                        squareColor = Color.BLUE;
-                }
+				switch (curCell) {
+				case GOAL:
+					squareColor = Color.RED;
+					break;
+				case START:
+					squareColor = Color.GREEN;
+					break;
+				case EMPTY:
+					squareColor = Color.LIGHTGREY;
+					break;
+				case WALL:
+					squareColor = Color.BLUE;
+				}
 
-                Rectangle cellRect = new Rectangle();
-                cellRect.setStroke(Color.BLACK);
-                cellRect.setStrokeWidth(1);
-                cellRect.setX(col * squareSideLength);
-                cellRect.setY(row * squareSideLength);
-                cellRect.setWidth(squareSideLength);
-                cellRect.setHeight(squareSideLength);
-                cellRect.setFill(squareColor);
+				Rectangle cellRect = new Rectangle();
+				cellRect.setStroke(Color.BLACK);
+				cellRect.setStrokeWidth(1);
+				cellRect.setX(col * squareSideLength);
+				cellRect.setY(row * squareSideLength);
+				cellRect.setWidth(squareSideLength);
+				cellRect.setHeight(squareSideLength);
+				cellRect.setFill(squareColor);
 
-                root.getChildren().add(cellRect);
-                mazeRects[col][row] = cellRect;
-            }
-        }
+				root.getChildren().add(cellRect);
+				mazeRects[col][row] = cellRect;
+			}
+		}
 
-        ImageView robotImageView = new ImageView();
-        Robot robot = new Robot(robotImageView, path, maze.getStartingPos(), squareSideLength, mazeRects);
-        scoreIndicator.setmRobot(robot);
+		ImageView robotImageView = new ImageView();
+		Robot robot = new Robot(robotImageView, path, maze.getStartingPos(), squareSideLength, mazeRects);
+		scoreIndicator.setmRobot(robot);
 
-        primaryStage.setTitle("Simulator");
-        primaryStage.setResizable(false);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		primaryStage.setTitle("Simulator");
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 
-        prevTime = System.nanoTime();
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long curTime) {
-                double deltaTime = (curTime - prevTime) / 1E9;
-                //System.out.println(1 / deltaTime);
-                onUpdate(deltaTime);
-                prevTime = curTime;
-            }
-        };
+		prevTime = System.nanoTime();
+		AnimationTimer timer = new AnimationTimer() {
+			@Override
+			public void handle(long curTime) {
+				double deltaTime = (curTime - prevTime) / 1E9;
+				// System.out.println(1 / deltaTime);
+				onUpdate(deltaTime);
+				prevTime = curTime;
+			}
+		};
 
-        timer.start();
+		timer.start();
 
-        restartButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                timer.stop();
-                path.resetIterator();
-                openSimulation(maze, path);
-            }
-        });
+		restartButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				timer.stop();
+				path.resetIterator();
+				openSimulation(maze, path);
+			}
+		});
 
-        otherMapButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                timer.stop();
-                primaryStage.hide();
-                openSelectMapPopUp();
-            }
-        });
-    }
+		otherMapButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				timer.stop();
+				primaryStage.hide();
+				openSelectMapPopUp();
+			}
+		});
+	}
 
-    private void onUpdate(double deltaTime) {
-        for (Sprite sprite : allSpriteList) {
-            sprite.update(deltaTime);
-        }
-    }
+	private void onUpdate(double deltaTime) {
+		for (Sprite sprite : allSpriteList) {
+			sprite.update(deltaTime);
+		}
+	}
 }
